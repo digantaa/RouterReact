688cab68879ff84731a1e02a4ca59249c4e462b5
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-09-17 21:38:59 +0530

    debugging

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {RouteProvider, createBrowserRoute} from 'react-router-dom'
 import Layout from './Layout';
+import Home from './components/Home';
+import About from './components/About';
 
 const router = createBrowserRoute([
   {
@@ -13,7 +15,10 @@ const router = createBrowserRoute([
         path:"",
         element: <Home/>
       }, 
-      {}
+      {
+        path:"about",
+        element: <About/>
+      }
     ]
   }
 ])
