262227ea0b93e4a1767714ed9400975432f630c6
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-09-17 21:11:37 +0530

    adding home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
+
 import './App.css'
+import Header from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className=''>
-        
-      </div>
+      <Header/>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link, NavLink} from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -13,14 +13,31 @@ const Header = () => {
 
                 <div className="items-center flex lg:order-2">
                     <Link className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg
-                    text-sm px-4 lg;px-5 py-2 lg:py-2.5 mr-2 focus:outline-non" to='#'>
+                    text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" to='/login'>
                         Log in
                     </Link>
 
                     <Link className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300
-                    font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">
-                    
+                    font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" to='/signup'>
+                        Get started
                     </Link>
+                </div>
+                <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                    <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                        <li>
+                            <NavLink 
+                                to='/' 
+                                className={({isActive}) => 
+                                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50
+                                    lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${
+                                        isActive ? 'text-orange-700' : 'text-gray-700'
+                                    }`
+                                }
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                    </ul>
                 </div>
 
             </div>
