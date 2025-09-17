9dfca313522f62ab403f0cef1b2541bee4126bf2
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-09-17 21:29:31 +0530

    making about component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {RouteProvider} from 'react-router-dom'
+import {RouteProvider, createBrowserRoute} from 'react-router-dom'
+import Layout from './Layout';
 
+const router = createBrowserRoute([
+  {
+    path:'/',
+    element: <Layout/>,
+    children: [
+      {
+        path:"",
+        element: <Home/>
+      }, 
+      {}
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouteProvider/>
+    <RouteProvider router={router}/>
   </StrictMode>,
 )
