6997b83da7954067d4bf80d665efd7db31a60672
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-09-17 21:17:07 +0530

    making routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {RouteProvider} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouteProvider/>
   </StrictMode>,
 )
