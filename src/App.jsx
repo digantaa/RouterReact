f051c3c37f8ef86f380c34b21a3e88821dd6531c
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-09-17 21:13:32 +0530

    adding routing properties in header home and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-
 import './App.css'
 import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
       <Header/>
+      <Home/>
+      <Footer/>
     </>
   )
 }
